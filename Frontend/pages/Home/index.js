1b4b74a281d66a8f982caa63170636dfe0d42d71
Jinming Shi <lkwinderman@gmail.com>
2022-10-06 10:46:17 -0400

    homepage structure

--- Frontend/pages/Home/index.js
+++ Frontend/pages/Home/index.js
@@ -1,20 +1,114 @@
 import React from 'react';
-import {Text, StyleSheet, ScrollView} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
+import {ListItem, Avatar, Chip} from '@rneui/themed';
 
 const Home = () => {
   return (
     <ScrollView style={styles.scrollView}>
-      <Text style={styles.text}>Home</Text>
+      <View style={{paddingBottom: 15}}>
+        {list.map((l, i) => (
+          <ListItem key={i} bottomDivider style={styles.listItem}>
+            <Avatar
+              source={
+                l.avatar_url
+                  ? {uri: l.avatar_url}
+                  : require('../../static/Images/Home/default-people.jpg')
+              }
+            />
+            <ListItem.Content>
+              <ListItem.Title>{l.name}</ListItem.Title>
+              <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+              {/* tags */}
+              <View style={styles.tagContainer}>
+                {l.tags &&
+                  l.tags.map((element, index) => (
+                    <View
+                      style={{
+                        borderRadius: 4,
+                        backgroundColor: '#2387df',
+                        marginRight: 5,
+                        marginBottom: 3,
+                      }}>
+                      <Text style={styles.tag} key={index}>
+                        {element}
+                      </Text>
+                    </View>
+                  ))}
+              </View>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+      </View>
     </ScrollView>
   );
 };
 
+const list = [
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice President',
+    tags: ['swimming', 'basketball', 'swimming', 'basketball', 'swimming', 'basketball'],
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice Chairman',
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice President',
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice Chairman',
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice President',
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice Chairman',
+  },
+  {
+    name: 'Amy Farha',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice President',
+  },
+  {
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://images.pexels.com/photos/746386/pexels-photo-746386.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    subtitle: 'Vice Chairman',
+  },
+];
+
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: '#888888',
+    flex: 1,
   },
-  text: {
-    fontSize: 42,
+  tagContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  tag: {
+    padding: 3,
+    color: '#fff',
+  },
+  listItem: {
+    marginTop: 5,
   },
 });
 
