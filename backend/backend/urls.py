31d1157819f14dd6c4bf51d02461a7789bc815de
Ubuntu <ubuntu@ip-172-31-80-23.ec2.internal>
2022-10-10 18:09:12 +0000

    changed re_path to path, changed to include("django.contrib.auth.urls")

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'api/', include('api.urls')),
+    path('api/', include("django.contrib.auth.urls")),
 ]
