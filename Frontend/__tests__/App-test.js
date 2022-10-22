38e439927b853592c1f8906fa5a7d18532f3f6d0
Jinming Shi <lkwinderman@gmail.com>
2022-10-22 16:25:41 -0400

    add jtests

--- Frontend/__tests__/App-test.js
+++ Frontend/__tests__/App-test.js
@@ -9,6 +9,18 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
+
+test('renders correctly', () => {
+  expect(mockedNavigate).toHaveBeenCalledTimes(0);
 });
