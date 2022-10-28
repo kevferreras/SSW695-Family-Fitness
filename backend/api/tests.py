98dd603eb7051f9bdad47f27b514bb8c7f9f6d7b
kevferreras <kevferreras11@gmail.com>
2022-10-28 15:03:04 -0400

    backend testing update

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+import unittest
+import views
+
+class CreateUserAPIViewTest(unittest.TestCase):
+    
+    def test_create(self) -> None:
+        self.assertAlmostEqual()
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(exit = False, verbosity = 2)
