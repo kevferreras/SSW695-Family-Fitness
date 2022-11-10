e4e0ca3651880e94f173bc3abc0fff47b31936d5
Kevin Mengarelli <mango23322@gmail.com>
2022-11-09 19:02:07 -0600

    Revert "update to test_models"
    This reverts commit 26a6a6c5ed062fc3dde387dc55a92f588d027119.

--- backend/api/tests/test_models.py
+++ backend/api/tests/test_models.py
@@ -108,7 +108,7 @@ class PostModelTest(TestCase):
     def test_post_name_label(self):
         post = Post.objects.get(id=1)
         field_label = post._meta.get_field('name').verbose_name
-        self.assertEqual(field_label, 'Post')        
+        self.assertEqual(field_label, 'name')        
 
     def test_post_name_max_length(self):
         post = Post.objects.get(id=1)
@@ -137,7 +137,12 @@ class PostModelTest(TestCase):
 
     def test_post_value(self):
         post = Post.objects.get(id=1)
-        post_name = getattr(post, 'post')
+        post_name = getattr(post, 'name')
+        self.assertEqual(post_name, 'Running 5K')
+
+    def test_post_name_value(self):
+        post = Post.objects.get(id=1)
+        post_name = getattr(post, 'post_name')
         self.assertEqual(post_name, 'Running my 1st 5K today')
 
     def test_post_date_value(self):
@@ -162,11 +167,11 @@ class CommentModelTest(TestCase):
     def test_comment_label(self):
         comment = Comment.objects.get(id=1)
         field_label = comment._meta.get_field('comment').verbose_name
-        self.assertEqual(field_label, 'comment')   
+        self.assertEqual(field_label, 'Comment')   
 
     def test_comment_name_value(self):
         comment = Comment.objects.get(id=1)
-        comment_name = getattr(comment, 'name')
+        comment_name = getattr(comment, 'comment_name')
         self.assertEqual(comment_name, 'Great work')
 
     def test_comment_value(self):
@@ -181,7 +186,7 @@ class PhotoModelTest(TestCase):
     def test_photo_name_label(self):
         photo = Photo.objects.get(id=1)
         field_label = photo._meta.get_field('name').verbose_name
-        self.assertEqual(field_label, 'Photo')
+        self.assertEqual(field_label, 'name')
 
     def test_photo_name_value(self):
         photo = Photo.objects.get(id=1)
